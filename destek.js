af591bf8c709af5412e70dce6fe76059caabcb2b
Glitch (chief-locrian-railway) <none>
2020-04-03 10:50:45 +0000

    ☔️🍣 Updated with Glitch

---
+++ destek.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+
+exports.run = async (client, message, params) => {
+    if(message.channel.type == "dm")  return;
+  if(message.channel.type !== "text") return;
+  var channel = client.channels.find('id', '694994494281613322')
+    const asdf = await client.channels.get(message.channel.id).createInvite()
+  message.delete();
+  const embed = new Discord.RichEmbed()
+  .setTitle("» Engin'in Kedicikleri Bot | Canlı Destek")
+  .setDescription("**Canlı Desteği kullandğın için teşekkür ederiz, Seninle ekibim ilgilenicektir lütfen bekle!**")
+  .setFooter("30 Saniye içinde geri dönülmezse lütfen iletişime geçin <@695211672448532510>")
+ message.channel.send(embed)
+      const invite = new Discord.RichEmbed()
+  .setAuthor("» Canlı Destek | Talep")
+  .addField('**» Kullanıcı Adı: **', message.author.username + '#' + message.author.discriminator)
+  .addField('**» Sunucu Adı: **', message.guild.name)
+  .setDescription(asdf.url)
+      channel.send(invite)
+};
+  
+  exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['destek'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'destek',
+  description: 'Canlı Destek Tablebi Oluşturur.',
+  usage: 'destek'
+};
